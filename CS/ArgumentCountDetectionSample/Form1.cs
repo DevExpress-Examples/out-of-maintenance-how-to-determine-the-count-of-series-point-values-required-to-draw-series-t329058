1c28c76d745a749cfc2fd2b3ec710e6724a2cf8f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ArgumentCountDetectionSample/Form1.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.XtraCharts;
+using System;
+using System.Windows.Forms;
+
+namespace ArgumentCountDetectionSample {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        #region #ValuesCount
+        private void chartControl1_ObjectSelected(object sender, HotTrackEventArgs e) {
+            if (e.HitInfo.InSeries) {
+                int count = e.HitInfo.Series.View.ValuesCount;
+                MessageBox.Show(this, String.Format("The selected series is ploted using {0} series point values.", count));
+            }
+        }
+        #endregion #ValuesCount
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to: determine the count of series point values required to draw series
+
+
+This example demonstrates how to determine the count of series point values required to draw series of a specific view type.
+
+
+<h3>Description</h3>
+
+To determine the count of values required to plot series with a specific view, use the&nbsp;<a href="https://documentation.devexpress.com/#CoreLibraries/DevExpressXtraChartsSeriesViewBase_ValuesCounttopic">SeriesViewBase.ValuesCount</a>&nbsp;property.
+
+<br/>
+
+
